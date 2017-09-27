6abea78580fbd18e1a6d044d040c382f4fd41a93
Andrew H <andrewh717@gmail.com>
2017-09-27 11:02:15 -0400

    Initial attempt at mergeSort for strings

--- Part 0 - The Sorter/mergesort.c
+++ Part 0 - The Sorter/mergesort.c
@@ -1,7 +1,7 @@
 #include"sorter.h"
+#include<string.h>
 
-// Merge left and right sides into one array
-// Used in the mergeSort method
+// Mergesort for array of integers
 void mergeInt(int *left, int *right, int *arr, int sizeLeft, int sizeRight) {
     int i = 0, j = 0, k = 0;
     while (i < sizeLeft && j < sizeRight) {
@@ -20,7 +20,6 @@ void mergeInt(int *left, int *right, int *arr, int sizeLeft, int sizeRight) {
     }
 }
 
-// Recursive method of performing mergeSort on an array of ints
 void mergeSortInt(int *arr, int size) {
     int *left, *right, i;
     if (size < 2) {
@@ -29,8 +28,8 @@ void mergeSortInt(int *arr, int size) {
     int mid = size / 2;
     int sizeLeft = mid;
     int sizeRight = size - mid;
-    left = (int*) malloc (sizeLeft * sizeof(int));
-    right = (int*) malloc (sizeRight * sizeof(int));;
+    left = (int*) malloc(sizeLeft * sizeof(int));
+    right = (int*) malloc(sizeRight * sizeof(int));;
     for (i = 0; i < mid; i++) {
         left[i] = arr[i];
     }
@@ -44,9 +43,53 @@ void mergeSortInt(int *arr, int size) {
     free(right);
 }
 
+// Mergesort for array of strings
+void mergeString(char **left, char **right, char **arr, int sizeLeft, int sizeRight){
+    int i = 0, j = 0, k = 0;
+    while (i < sizeLeft && j < sizeRight) {
+        if (strcmp(left[i], right[i]) < 0) {
+            strcpy(arr[k++], left[i++]);
+        } else {
+            strcpy(arr[k++], right[j++]);
+        }
+    }
+    while (i < sizeLeft) {
+        strcpy(arr[k++], left[i++]);
+    }
+    while (j < sizeRight) {
+        strcpy(arr[k++], right[j++]);
+    }
+}
+
+void mergeSortString(char **str, int length) {
+    char **left, **right, i;
+    if (length < 2) {
+        return;
+    }
+    int mid = length /2;
+    int sizeLeft = mid;
+    int sizeRight = size - mid;
+    left = (char**) malloc(sizeLeft * sizeof(char *));
+    right = (char**) malloc(sizeRight * sizeof(char *));;
+    for (i = 0; i < mid; i++) {
+        left[i] = malloc(sizeof(str[i]));
+        strcpy(left[i], str[i]);
+    }
+    for (i = mid; i < size; i++) {
+        right[i - mid] = str[i];
+        strcpy(right[i - mid], str[i]);
+    }
+    mergeSortString(left, sizeLeft);
+    mergeSortString(right, sizeRight);
+    mergeString(left, right, arr, sizeLeft, sizeRight);
+    free(left);
+    free(right);
+}
+
+// For testing purposes
 int main() {
     int i;
-	// Declare and initialze array of integers to be sorted
+	// Declare and initialize array of integers to be sorted
     int a[] = {1,5,3,6,88,11,2};
 	// Need to get the size of the array now because it needs to be done in scope of the array declaration
 	int size = sizeof(a)/sizeof(a[0]);
@@ -63,6 +106,26 @@ int main() {
     for(i = 0; i < (size); i++) {
         printf("%d ", a[i]);
     }
-	printf("\n");
+    printf("\n");
+    
+    // Testing mergeSortString
+    int numStrings;
+    printf("\nPlease enter the number of strings to be sorted:\n");
+    scanf("%d", numStrings);
+    char **strings = (char **) malloc(numStrings * sizeof(char *));
+    printf("Enter the strings:\n");
+    for(i = 0; i < numStrings; i++){
+        strings[i] = malloc(sizeof(char) * 10);
+        scanf("%s", strings[i]);
+    }
+    mergeSortString(strings, numStrings);
+    printf("\nHere is the sorted array of strings:\n");
+    for(i = 0; i < numStrings; i++){
+        if(i == (numStrings - 1)){
+            printf("%s", strings[i]);
+        } else {
+            printf("%s, ", strings[i]);
+        }        
+    }
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# cs-214
+# CS-214: Systems Programming
+Compile with: ```gcc -g -Wall -Werror -fsanitize=address fileName.c -o fileName```
