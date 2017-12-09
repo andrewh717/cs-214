1994fce962664a04b23caa10ce9be7ced5294f2f
Andrew Hernandez <andrewh717@gmail.com>
2017-12-09 13:43:22 -0500

    Initial commit for Part 3

---
+++ Part 3 - Server_Client/sorter_server.h
@@ -0,0 +1,49 @@
+// Authors: Andrew Hernandez & Michael Belmont
+// CS214: Systems Programming Fall 2017
+#ifndef SORTER_SERVER_H_
+#define SORTER_SERVER_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<dirent.h>
+#include<pthread.h>
+#include<unistd.h>
+
+/* Socket Stuff */
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+typedef struct record {
+	char **line;
+	int numColumns;
+} record;
+
+typedef struct csvfile {
+	record *table;
+	int numRows;
+} csvfile;
+
+typedef struct LL {
+	struct LL *next;
+	csvfile data;
+} LL;
+
+typedef struct tid_type
+{
+	pthread_t tid;
+	int isFree;
+	int socketfd;
+}tid_type;
+
+void trim(char *str);
+
+int checkForDuplicates(char **str, int numStrings);
+
+int getColumnIndex(record *arr, int numColumns, char *columnName);
+
+int get_tid();
+
+#endif // SORTER_SERVER_H_
